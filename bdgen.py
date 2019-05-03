e680948ae074364e1ba366d91076e01508a29f22
tletutour <tom.le-tutour@insa-lyon.fr>
2019-05-03 14:12:18 +0200

    Ajout méthodes API bd + petites modif à la base

--- bdgen.py
+++ bdgen.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer,Float, String,MetaData ,ForeignKey,DateTime,Binary
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 
 
-engine = create_engine('sqlite:///:memory:', echo=True)   
+engine = create_engine('sqlite:///base.db', echo=False)   
 
 metadata = MetaData()
 
@@ -37,18 +35,63 @@ topic=Table('topic',metadata,
 
 matiere=Table('matiere',metadata,
     Column('id' ,Integer,autoincrement=True, primary_key=True),
-    Column('total_size',Float),
+    Column('nom_matiere',String),
+    Column('contenu',String),
+    Column('score',Integer),
+    Column('refere',Integer) ,
+    Column('time',DateTime),
+    Column('idUser',Integer, ForeignKey("user.id")),
+    Column('contenu',String),
+    Column('score',Integer),
+    Column('refere',Integer) ,
+    Column('time',DateTime),
+    Column('idUser',Integer, ForeignKey("user.id")),
+    Column('idTopic',Integer, ForeignKey("topic.id")),
     Column('weekly_score',Integer)
 )
         
 file=Table('file',metadata,
     Column('id',Integer,autoincrement=True, primary_key=True),
+    Column('nom_fichier',String),
     Column('size',Float),
-    Column('data',Binary),
+    Column('data',Binary,nullable=False),
     Column('file_type',String),
-    Column('idMatiere',Integer, ForeignKey("matiere.id"))
+    Column('id_matiere',Integer, ForeignKey("matiere.id"))
 )
+
+def ajouter_matiere(nom_mat,conn):
+    mat_ins = matiere.insert()
+    conn.execute(mat_ins.values(nom_matiere=nom_mat,weekly_score=0))
+"""  
+def lister_fich_mat(id_mat, conn):
+    resultat=.query    
+"""    
+def ajouter_fichier(filepath,nom_fich,id_mat,conn):
+    fich_ins = file.insert()
+    try:
+        with open(filepath,'rb') as binary_file:
+            print(type(binary_file))
+            data = binary_file.read()
+            conn.execute(fich_ins.values(nom_fichier=nom_fich,
+                                         size=os.stat(filepath).st_size,
+                                         data=data,
+                                         file_type=None,
+                                         id_matiere=id_mat))
+            binary_file.close()
+        return True
+    except IOError:
+        return False #Couldn't open File!
+    
     
 
 metadata.create_all(engine)                               
-connection = engine.connect()
+connection = engine.connect() 
+
+
+ajouter_matiere("WEB",connection)
+ajouter_matiere("CRO",connection)
+ajouter_matiere("TSA",connection)
+ajouter_fichier("/home/tom/Documents/certificate.pdf","sujet de web",1,connection)
+for row in connection.execute("select * from file"):
+    print(row)
+print('\n')
